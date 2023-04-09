efbb9c4823aa64fe281cf015e378bca1ff6b54d7
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-08 23:36:04 -0700

    input components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import Button from "@ui/Button";
 import { FC, useState } from "react";
+import { AirVent } from "lucide-react";
 
 interface LoginPageProps {}
 
@@ -11,13 +12,31 @@ const LoginPage: FC<LoginPageProps> = ({}) => {
   return (
     <>
       <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 md:px-8">
-        <div className="flex w-full max-w-md flex-col items-center space-y-8 ">
-          <div className="flex flex-col items-center gap-8">
-            Logo
-            <h2 className="text-3-xl mt-6 text-center font-bold tracking-tight text-gray-900">
+        <div className="flex w-full max-w-md flex-col items-center gap-5 ">
+          <div className="flex flex-col items-center">
+            <AirVent />
+            <h2 className="text-3-xl mt-3 text-center font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
           </div>
+
+          <label className="flex w-full max-w-xs flex-col text-md">
+            Email
+            <input
+              type="text"
+              placeholder="email@example.com"
+              className="rounded-md  border border-slate-400 py-1 px-2 placeholder:opacity-50 placeholder:text-sm focus:outline-none" 
+            />
+          </label>
+
+          <label className="flex w-full max-w-xs flex-col text-md">
+            Password
+            <input
+              type="password"
+              placeholder="password"
+              className="rounded-md  border border-slate-400 py-1 px-2 placeholder:opacity-50 placeholder:text-sm focus:outline-none" 
+            />
+          </label>
 
           <Button
             isLoading={isLoading}
